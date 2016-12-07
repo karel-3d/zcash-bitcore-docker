dfb8a658f0263993d5e9bce5eaa8f737e76c2154
Karel Bilek <kb@karelbilek.com>
2016-12-07 09:12:46 +0100

    New build, with zcash-cli

--- build/run.sh
+++ build/run.sh
@@ -4,6 +4,7 @@ docker build -t zcash-build .
 docker run -t \
     -v $(pwd)/res:/res \
     zcash-build \
-    /bin/sh -c "cd zcash && ./zcutil/build.sh -j"$(nproc)" && cp src/zcashd /res"
+    /bin/sh -c "cd zcash && ./zcutil/build.sh -j"$(nproc)" && cp src/zcashd /res && cp src/zcash-cli /res"
 
 cp res/zcashd ../
+cp res/zcash-cli ../
